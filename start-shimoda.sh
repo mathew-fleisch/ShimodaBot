7afb2934a7ac4386c458e52e2c69b9fc37df64ed
Mathew Fleisch <Mathew@MacBook-Pro-26.local>
2017-07-20 13:50:18 -0700

    This script now only connects to the irc network and channel. Starting the bot has been decoupled and therefore doesn’t need to be a required parameter.

--- start-shimoda.sh
+++ start-shimoda.sh
@@ -55,24 +55,27 @@ TARGETPATH=$(echo ${TARGETPATH} | sed -e 's/\/$//g')
 # Main loop
 if [[ -d ${TARGETPATH} ]]; then
 	echo "Starting: ${TARGETPATH}"
-	if [ -e "$BOTPATH" ]; then
-		echo "Bot: ${BOTPATH}"
-		ii -s ${NETWORK} -n ${BOTNAME} &
-		IIPID=$(pidof ii)
-		echo "Connected to irc: ${IIPID}"
-		if [[ -d ${TARGETPATH}/${NETWORK} ]]; then
-			# Log into channel
-			echo "/j ${CHANNEL}" > ${TARGETPATH}/${NETWORK}/in
-			sleep 2
-			tail -n9 ${TARGETPATH}/${NETWORK}/out
-			# echo "${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in" # DEBUG
-			echo "${BOTNAME} reporting for duty!" > ${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in
-			echo "Run ${BOTNAME}"
+	ii -s ${NETWORK} -n ${BOTNAME} &
+	IIPID=$(pidof ii)
+	echo "Connected to irc: ${IIPID}"
+	if [[ -d ${TARGETPATH}/${NETWORK} ]]; then
+		# Log into channel
+		echo "/j ${CHANNEL}" > ${TARGETPATH}/${NETWORK}/in
+		sleep 2
+		tail -n9 ${TARGETPATH}/${NETWORK}/out
+		# echo "${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in" # DEBUG
+		echo "${BOTNAME} reporting for duty!" > ${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in
+		echo "Run ${BOTNAME}"
+
+		if [ -e "$BOTPATH" ]; then
+			echo "Bot: ${BOTPATH}"
 			echo "${BOTPATH} -d ${TARGETPATH}/${NETWORK}/${CHANNEL,,}"
+		else 
+			echo "Must specify bot location... Guessing:"
+			echo "./ShimodaBot.sh -d ${TARGETPATH}/${NETWORK}/${CHANNEL,,}"
 		fi
-	else 
-		echo "Must specify bot location"
 	fi
+
 else 
 	echo "Must specify ii path..."
 fi
