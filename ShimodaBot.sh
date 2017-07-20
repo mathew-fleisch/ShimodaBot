8eb2745d7f8cacf93f30896214dbc3f815ed48e2
Mathew Fleisch <mathew.fleisch@gmail.com>
2017-07-19 21:15:14 -0700

    First commit

---
+++ ShimodaBot.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Dependencies
+command -v inotifywait >/dev/null 2>&1 || { echo "This script requires inotify-tools, but it is not installed.  Aborting." >&2; exit 1; }
+
+# Globals
+DESTINATION=""
+declare -A SIMPLE_MESSAGES=( ["What's up ShimodaBot"]="I'm Drunk..." ["Hey"]="Howdy" )
+
+# Get target destination path
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -d|--destination)
+	DESTINATION="$2"
+	shift
+	;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
+# Remove trailing slash from target destination path
+DESTINATION=$(echo ${DESTINATION} | sed -e 's/\/$//g')
+
+# Main loop
+if [[ -d ${DESTINATION} ]]; then
+	# echo $DESTINATION; # DEBUG
+	while inotifywait -e modify ${DESTINATION}/out; do
+		NEWMSG=$(tail -n1 ${DESTINATION}/out)
+		# echo "NEWMSG: ${NEWMSG}" # DEBUG
+		for user_message in "${!SIMPLE_MESSAGES[@]}"; do 
+			echo "$user_message - ${SIMPLE_MESSAGES[$user_message]}"
+			if echo "${NEWMSG}" | grep "${user_message}"; then
+				echo "${SIMPLE_MESSAGES[$user_message]}" > ${DESTINATION}/in
+				break 1;
+			fi
+		done
+	done
+else
+	echo "Directory not found: ${DESTINATION}"
+fi
---
+++ start-shimoda.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Dependencies
+command -v ii >/dev/null 2>&1 || { echo "This script requires ii, but it is not installed.  Aborting." >&2; exit 1; }
+command -v pidof >/dev/null 2>&1 || { echo "This script requires pidof, but it is not installed.  Aborting." >&2; exit 1; }
+
+# Globals
+BOTPATH=""
+TARGETPATH=""
+NETWORK="chat.freenode.net"
+CHANNEL="#GreatestGen"
+BOTNAME="ShimodaBot"
+# Get target path path
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -b|--bot-path)
+	BOTPATH="$2"
+	shift
+	;;
+    --bot-name)
+	BOTNAME="$2"
+	shift
+	;;
+    -c|--channel)
+	CHANNEL="$2"
+	shift
+	;;
+    -p|--path)
+	TARGETPATH="$2"
+	shift
+	;;
+    -n|--network)
+	NETWORK="$2"
+	shift
+	;;
+    --default)
+    DEFAULT=YES
+    ;;
+    *)
+    # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 $1
+fi
+# Remove trailing slash from target path path
+TARGETPATH=$(echo ${TARGETPATH} | sed -e 's/\/$//g')
+
+# Main loop
+if [[ -d ${TARGETPATH} ]]; then
+	echo "Starting: ${TARGETPATH}"
+	if [ -e "$BOTPATH" ]; then
+		echo "Bot: ${BOTPATH}"
+		ii -s ${NETWORK} -n ${BOTNAME} &
+		IIPID=$(pidof ii)
+		echo "Connected to irc: ${IIPID}"
+		if [[ -d ${TARGETPATH}/${NETWORK} ]]; then
+			# Log into channel
+			echo "/j ${CHANNEL}" > ${TARGETPATH}/${NETWORK}/in
+			sleep 2
+			tail -n9 ${TARGETPATH}/${NETWORK}/out
+			# echo "${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in" # DEBUG
+			echo "${BOTNAME} reporting for duty!" > ${TARGETPATH}/${NETWORK}/${CHANNEL,,}/in
+			echo "Run ${BOTNAME}"
+			$(${BOTPATH} -d ${TARGETPATH}/${NETWORK}/${CHANNEL,,} &)
+		fi
+	else 
+		echo "Must specify bot location"
+	fi
+else 
+	echo "Must specify ii path..."
+fi
+
